a5b9472acccb6636941aa35cf8c1721b5443c833
Md Asif <mdasiff007@gmail.com>
2023-06-20 16:48:36 +0530

    .env

--- src/custom/config.js
+++ src/custom/config.js
@@ -1,6 +1,5 @@
 let dev_url = 'http://127.0.0.1:8000/';
 let server_url = 'https://av-ecom-cms.avdemosites.com/';
-const mode = 'development';
 
 const dev = {
     API_URL: dev_url+"api/",
@@ -11,5 +10,6 @@ const server = {
     API_URL: server_url+"api/",
     ASSET_URL: server_url+"api/"
 }
-const config = mode === 'development' ? dev : server;
+const config = process.env.NODE_ENV === 'development' ? dev : server;
+console.log(config);
 export default config;
